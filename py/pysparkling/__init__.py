b9d1e34541d7830fab357c2e7609e8e37547769e
Jakub Hava <jakub.hava@gmail.com>
2017-10-25 15:25:20 +0200

    Fix __init__

--- py/pysparkling/__init__.py
+++ py/pysparkling/__init__.py
@@ -10,7 +10,6 @@ __version__ = "SUBST_PROJECT_VERSION"
 from pysparkling.conf import H2OConf
 # set imports from this project which will be available when the module is imported
 from pysparkling.context import H2OContext
-from pysparkling.ml.feature import ColumnPruner
 
 # set what is meant by * packages in statement from foo import *
-__all__ = ["H2OContext", "H2OConf", "ColumnPruner"]
+__all__ = ["H2OContext", "H2OConf"]
