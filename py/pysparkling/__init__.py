c654b39098493f8b0ae17b15543f73e939cd7068
Jakub Hava <jakub.hava@gmail.com>
2017-10-25 11:24:35 +0200

    [SW-519] Introduce SW Models into Spark python pipelines

--- py/pysparkling/__init__.py
+++ py/pysparkling/__init__.py
@@ -10,6 +10,7 @@ __version__ = "SUBST_PROJECT_VERSION"
 # set imports from this project which will be available when the module is imported
 from pysparkling.context import H2OContext
 from pysparkling.conf import H2OConf
+from pysparkling.feature import ColumnPruner
 
 # set what is meant by * packages in statement from foo import *
-__all__ = ["H2OContext", "H2OConf"]
+__all__ = ["H2OContext", "H2OConf", "ColumnPruner"]
